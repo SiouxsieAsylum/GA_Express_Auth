4c0530bafdfd3febcd617273a6bcd6229039fccc
J Silverstein <jess.pebble@gmail.com>
2017-10-23 18:37:52 -0400

    add middlewares

--- flashcards-final/server.js
+++ flashcards-final/server.js
@@ -3,8 +3,12 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -19,6 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index');
